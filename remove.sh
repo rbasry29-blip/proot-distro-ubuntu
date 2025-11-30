8749eba9ad71583b9289c6b2a6650dd0efd92e8b
rbasry29-blip <rbasry29@gmail.com>
2025-11-30 08:31:06 +0600

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,8 +1,10 @@
 c='\e[1;96m'
+r='\e[0m'
 clear
 printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
+printf "${c}Removing UBUNTU ${r}"
 proot-distro remove ubuntu
 apt remove proot-distro x11-repo termux-x11-nightly -y
