521ce5fa9cc5fca03975c73b559539aa205bbcf1
rbasry29-blip <rbasry29@gmail.com>
2025-12-02 07:49:09 +0600

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -4,18 +4,24 @@ b='\e[34m'
 r='\e[0m'
 y='\e[1;33m'
 c='\e[1;96m'
-a(){
+
 
 clear
 printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${c} Updating and Installing packages (xubuntu-desktop and dbus-x11) This may take up to 2 hours,  please wait${r}\n"
+printf "${b}[${g}*${b}]${c} Updating and Installing packages.  This may take up to 2 hours,  please wait${r}\n"
 
 # Login to Ubuntu and run commands sequentially
-proot-distro login ubuntu -- bash -c "apt update -y  && dpkg --configure -a && apt update -y && apt install xubuntu-desktop dbus-x11 -y && printf "${b}[${g}*${b}]${g} Adding user ubuntu ${r}\n" 
- adduser ubuntu "
+proot-distro login ubuntu -- a(){
+apt update -y 
+dpkg --configure -a 
+apt update -y 
+apt install xubuntu-desktop dbus-x11 -y 
+printf "${b}[${g}*${b}]${g} Adding user ubuntu ${r}\n" 
+adduser ubuntu 
+} && a
 
 # Create gui launcher inside Ubuntu
 proot-distro login ubuntu -- bash -c "cat > /usr/bin/gui << 'EOF'
@@ -40,6 +46,3 @@ Then open another session and run -
 3. ${y}gui${g}
 Enjoy! ${r}\n"
 
-}
-
-a
