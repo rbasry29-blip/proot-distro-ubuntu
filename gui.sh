f261233ea74db615f09b34e99bb1a6ac4840e0fe
rbasry29-blip <rbasry29@gmail.com>
2025-12-03 20:53:12 +0600

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -11,10 +11,19 @@ printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${c} Updating and Installing packages.  This may take up to 2 hours,  please wait${r}\n"
+printf "${b}[${g}*${b}]${c} updating ${r}\n"
 
 # Login to Ubuntu and run commands sequentially
-proot-distro login ubuntu -- apt update -y 
+proot-distro login ubuntu -- apt update -y
+
+
+clear
+printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
+printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
+printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
+printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
+printf "${b}[${g}*${b}]${c} updating ${r}\n"
+
 proot-distro login ubuntu -- dpkg --configure -a 
 proot-distro login ubuntu -- apt update -y 
 proot-distro login ubuntu -- apt install xubuntu-desktop dbus-x11 -y
