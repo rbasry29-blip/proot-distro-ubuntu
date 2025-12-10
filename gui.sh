499366e633162ca6c3950359c5e9893a8f86de2f
linuxdevoloper <rbasry29@gmail.com>
2025-12-10 15:36:02 +0600

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -11,7 +11,7 @@ printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${c} Updating ${r}\n"
+printf "${b}[${g}*${b}]${c} Updating packages .....${r}\n"
 
 # Login to Ubuntu and run commands sequentially
 proot-distro login ubuntu -- apt update -y
