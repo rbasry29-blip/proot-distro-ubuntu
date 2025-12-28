7351d211f7caeb85fc040050e2afcc4a7718716c
linuxdevoloper <rbasry29@gmail.com>
2025-12-28 22:03:47 +0600

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -4,8 +4,32 @@ b='\e[34m'
 r='\e[0m'
 y='\e[1;33m'
 c='\e[1;96m'
-software(){
-dpkg --configure -a 
+commands(){                                                
+type_effect () {
+    local text="$1"
+    local delay=$2                                         
+    for ((i=0; i<${#text}; i++)); do                               printf "${c}${text:$i:1}${r}"
+        sleep "$delay"                                         done
+    echo                                                   }
+clear                                                      printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
+printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
+printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
+printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
+printf "$c"
+type_effect "Welcome" 0.04
+sleep 0.2
+type_effect "Hello , User" 0.08
+sleep 0.5
+type_effect "May your work will easier" 0.08
+sleep 0.7
+type_effect "Let's get started!" 0.08
+sleep 0.2
+type_effect "Enjoy proot-distro-ubuntu" 0.04
+
+printf "$r"
+sleep 2
+
+dpkg --configure -a
 apt --fix-broken install
 clear
 printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
@@ -30,7 +54,7 @@ printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
 
-printf "${b}[${g}*${b}]${g} Adding user ubuntu ${g}.....${r}\n" 
+printf "${b}[${g}*${b}]${g} Adding user ubuntu ${g}.....${r}\n"
 proot-distro login ubuntu -- adduser ubuntu
 
 printf "${b}[${g}*${b}]${g} Ubuntu installed! ${r}\n"
@@ -53,5 +77,6 @@ proot-distro login ubuntu --shared-tmp -- su - ubuntu
 EOF
 
 chmod +x $PREFIX/bin/ubuntu
+
 }
-software
+commands
