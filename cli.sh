1af9fe924d46fe9b125ac617e51898ff5c039b34
linuxdevoloper <rbasry29@gmail.com>
2025-12-29 14:18:43 +0600

    Refactor spinner function and update color codes

--- cli.sh
+++ cli.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-g='\e[92m'                                                       b='\e[34m'                                                       r='\e[0m'
-y='\e[1;33m'                                                     c='\e[1;96m'
-spinner() {                                                          local msg="$1"
-    shift                                                            local cmd="$*"                                                   local frames=( "∆___" "_∆__" "__∆_" "___∆" )
-    local i=0                                                                                                                         # Start spinner in background
-    (                                                                    while :; do
-            printf "\r${c}${msg} [${y}${frames[i]}${c}]"                     i=$(( (i + 1) % 4 ))                                             sleep 0.5                                                    done                                                         ) &                                                              local spin_pid=$!                                                                                                                 # Run all commands in foreground, hide output                    bash -c "$cmd" >/dev/null 2>&1                                   local cmd_status=$?                                          
+g='\e[1;92m'   # Green
+b='\e[34m'     # Blue
+r='\e[0m'      # Reset
+y='\e[1;33m'   # Yellow
+c='\e[1;96m'   # Light cyan
+
+spinner() {
+    local msg="$1"
+    shift
+    local cmd="$*"
+    local frames=( "∆___" "_∆__" "__∆_" "___∆" )
+    local i=0
+
+    # Start spinner in background
+    (
+        while :; do
+            printf "\r${c}${msg} [${y}${frames[i]}${c}]"
+            i=$(( (i + 1) % 4 ))
+            sleep 0.5
+        done
+    ) &
+    local spin_pid=$!
+
+    # Run all commands in foreground, hide output
+    bash -c "$cmd" >/dev/null 2>&1
+    local cmd_status=$?
+
     # Stop spinner
     kill "$spin_pid" >/dev/null 2>&1
     wait "$spin_pid" 2>/dev/null
@@ -47,9 +67,9 @@ printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${c} Updating and installing packages ${g}.....${r}\n"
+
 yes | termux-setup-storage
-spinner "${b}[${g}*${b}]${c} Installing packages${g}....." "apt update -y && apt install proot-distro -y && apt install x11-repo  -y && apt install termux-x11-nightly -y && sleep 3"
+spinner "${b}[${g}*${b}]${c} Installing packages${g}....." "apt update -y && apt install proot-distro -y && apt install x11-repo  -y && apt ins>
 sleep 1
 clear
 printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
